7c1ee6c3dd4ebc622da4e39ef21132d817d3b2d0
gayatri9826 <malthanegayatri07@gmail.com>
2026-06-23 12:40:49 +0530

    Add files via upload

---
+++ Method_Arguement.java
@@ -0,0 +1,12 @@
+class Method_Arguement {
+   static void addition(){
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println("addition:"+c);
+   } 
+   public static void main(String[] args) {
+     Method_Arguement m1=new Method_Arguement(); 
+    addition();
+  }
+}
