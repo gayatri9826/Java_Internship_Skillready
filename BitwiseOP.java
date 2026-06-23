fb76705fad5953285cd22dc262f4fa757cca26c8
gayatri9826 <malthanegayatri07@gmail.com>
2026-06-23 11:28:00 +0530

    Add files via upload

---
+++ BitwiseOP.java
@@ -0,0 +1,17 @@
+//Bitwise operator performs operations at binary level
+//01 01 01
+//bitwise and &
+//bitwise or |
+//bitwise XOR ^
+//bitwise left shift op <<
+//right shift op >>
+public class BitwiseOP {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        System.out.println("bitwise AND operator =" +(a&b));
+        System.out.println("bitwise OR operator =" +(a|b));
+        System.out.println("bitwise XOR operator =" +(a^b));
+    
+    }
+}
